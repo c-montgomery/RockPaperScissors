2679de48ba571969833585a89fdf662e95587ddf
c-montgomery <craym87@gmail.com>
2021-07-07 02:16:02 -0700

    created an game end condition

--- rps.js
+++ rps.js
@@ -12,30 +12,40 @@ rockButton.addEventListener('click', choose);
 paperButton.addEventListener('click', choose);
 scissorsButton.addEventListener('click', choose);
 
+let isPlaying = true;
+
+
+
 function choose(e){
-    switch(e.target.className){
-      case 'rock':
-        console.log('got to rock')
-        play('rock');
-        console.log('ran function')
-        break;
-      case 'paper':
-        play('paper');
-        break;
-      case 'scissors':
-        play('scissors');
-        break;
-      default:
-        console.log('ERROR, failure to select option');
+
+    if (playerScore + compScore < 5){
+        switch(e.target.className){
+        case 'rock':
+            console.log('got to rock')
+            play('rock');
+            console.log('ran function')
+            break;
+        case 'paper':
+            play('paper');
+            break;
+        case 'scissors':
+            play('scissors');
+            break;
+        default:
+            console.log('ERROR, failure to select option');
+        }
+    }else{
+        play('endGame')
     }
+    
 }
 
 
 //Returns random choice of RPS
 array = ['Rock', 'Paper', 'Scissors'];
 function computerPlay(){
+
     let compChoice = array[Math.floor(Math.random() * 3)];
-    
     return compChoice;
 }
 
@@ -47,38 +57,40 @@ let visibleCompScore= document.querySelector('.computer')
 //compares choices, chooses winner, increments score
 function gamePlay(playerSelection, computerSelection){
     
-
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        return('TIE!!!!! You chose ' + playerSelection +' and so did the computer')
+        return('TIE! You chose ' + playerSelection +' and so did the computer')
     } else if(playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() !== 'paper'){
         playerScore++;
         visiblePlayerScore.textContent = playerScore;
-        return ('player wins, ' + playerSelection + ' beats ' + computerSelection);   
+        return ('Player wins, ' + playerSelection + ' beats the computer\'s ' + computerSelection);   
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() !== 'scissors'){
         playerScore++;
         visiblePlayerScore.textContent = playerScore;
-        return ('player wins, ' + playerSelection + ' beats ' + computerSelection);
+        return ('Player wins, ' + playerSelection + '  beats the computer\'s ' + computerSelection);
     }else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() !=='rock'){
         playerScore++;
         visiblePlayerScore.textContent = playerScore;
-        return ('player wins, ' + playerSelection + ' beats ' + computerSelection);
+        return ('Player wins, ' + playerSelection + ' beats the computer\'s ' + computerSelection);
     }else{
         compScore++;
         visibleCompScore.textContent = compScore
-        return ('comp wins, ' + playerSelection + ' is beaten by ' + computerSelection)
+        return ('Computer chose ' + computerSelection +' beating your' + playerSelection  )
     }
 }   
-    
-
 
 //obtains player choice, informs of computer choice
 function play(choice){
-    if (playerScore + compScore < 5){
+    if (choice !== 'endGame'){
         let playerSelection = choice
         const computerSelection = computerPlay();
         let result = gamePlay(playerSelection, computerSelection)
         para.textContent = result;
-    } else{
-        para.textContent = "GAME OVER"
+    }else{
+        if (playerScore > compScore){
+            para.textContent = 'MAN OVER MACHINE!'
+        } else {
+            para.textContent = 'MACHINE OVER MAN'
+        }
     }
 }
+
